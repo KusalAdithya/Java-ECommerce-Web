f97b9d94be47ecccc365329be75b6f73715dd114
KusalAdithya <pererakusal79@gmail.com>
2024-12-20 13:24:57 +0100

    persistence xml created

--- src/main/java/com/waka/web/ecom/util/MySQLConnection.java
+++ src/main/java/com/waka/web/ecom/util/MySQLConnection.java
@@ -1,38 +1,38 @@
-package com.waka.web.ecom.util;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-public class MySQLConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD="Kusal_Adi/196572";
-    private static final String DATABASE = "ecom_db";
-
-    private static Connection c;
-
-    private static Statement createConnection() throws Exception {
-        if (c == null) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            c = (Connection) DriverManager.getConnection(URL + DATABASE, USERNAME, PASSWORD);
-        }
-
-        return c.createStatement();
-
-    }
-
-    public static void iud(String query) {
-        try {
-            createConnection().executeUpdate(query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static ResultSet search(String query) throws Exception {
-        return createConnection().executeQuery(query);
-    }
-
-}
+//package com.waka.web.ecom.util;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//
+//public class MySQLConnection {
+//    private static final String URL = "jdbc:mysql://localhost:3306/";
+//    private static final String USERNAME = "root";
+//    private static final String PASSWORD="Kusal_Adi/196572";
+//    private static final String DATABASE = "ecom_db";
+//
+//    private static Connection c;
+//
+//    private static Statement createConnection() throws Exception {
+//        if (c == null) {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            c = (Connection) DriverManager.getConnection(URL + DATABASE, USERNAME, PASSWORD);
+//        }
+//
+//        return c.createStatement();
+//
+//    }
+//
+//    public static void iud(String query) {
+//        try {
+//            createConnection().executeUpdate(query);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static ResultSet search(String query) throws Exception {
+//        return createConnection().executeQuery(query);
+//    }
+//
+//}
